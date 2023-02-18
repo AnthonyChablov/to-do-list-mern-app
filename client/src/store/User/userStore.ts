f1e425545317a6cea55c2bc072597e8f96967368
Anthony Chablov <achablov@myseneca.ca>
2023-02-17 23:30:28 -0500

    define user store

--- client/src/store/User/userStore.ts
+++ client/src/store/User/userStore.ts
@@ -1,16 +1,22 @@
 import create from 'zustand';
 
-
 type State = {
-
+    name: String,
+    email: String,
+    password: String,
 }
   
 type Action = {
-    
+    setName : (newName : String) => void,
+    setEmail : (newEmail : String)=>void,
+    setPassword : (newPassword : String)=> void,
 }
 
-
-
 export const useUserStore = create<State & Action>((set)=>({
-
+    name: '',
+    email: '',
+    password: '',
+    setName : (newName : String) => set({ name: newName }),
+    setEmail: (newEmail : String) => set({ email: newEmail }),
+    setPassword : (newName : String) => set({ name: newName }),
 }));
