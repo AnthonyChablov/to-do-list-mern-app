aea7179a6f8616a6206964a0aed829de371dfc33
Anthony Chablov <achablov@myseneca.ca>
2023-08-15 23:05:17 -0400

    change api config url

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,5 +1,5 @@
-export const API_URL = "https://to-do-list-mern-app-production.up.railway.app/api";
-export const API_CROSS_ORIGIN = 'https://to-do-list-mern-app-production.up.railway.app/';
+export const API_URL = "https://odd-erin-camel-tutu.cyclic.cloud/api";
+export const API_CROSS_ORIGIN = 'https://odd-erin-camel-tutu.cyclic.cloud/api';
 
 /* 
     export const API_URL = "https://to-do-list-mern-app-production.up.railway.app/api";
